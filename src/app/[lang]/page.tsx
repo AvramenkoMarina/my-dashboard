1c8931ed296fc41ea403b092a981838d89ac5ab7
AvramenkoMarina <marina.avramenko123@gmail.com>
2025-12-11 15:31:59 +0200

    add LangLayout and Page component with i18n support

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
+import { TEXT } from "@/i18n";
+import { Lang } from "../../../types";
+
+interface PageProps {
+  params: Promise<{ lang: Lang }>;
+}
+
+export default async function Page({ params }: PageProps) {
+  const { lang } = await params;
+  const t = TEXT[lang] ?? TEXT.en;
+
   return <div></div>;
 }
